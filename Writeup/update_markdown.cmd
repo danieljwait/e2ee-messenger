b7bd4d1a8d3fb101aa6645266a66d51cb6f5d5cf
Daniel Wait <71983137+daniel95wait@users.noreply.github.com>
2021-03-09 15:51:02 +0000

    Writeup: Rename md, add variables to script
    writeup.md has been renamed to README.md so that it is shown in
    GitHub when the Writeup directory is viewed
    
    In update_markdown.cmd, names for input and output files are no
    longer hardcoded

--- Writeup/update_markdown.cmd
+++ Writeup/update_markdown.cmd
@@ -2,21 +2,24 @@
 CD "%~dp0%"
 TITLE Update Markdown
 
+SET input=writeup.docx
+SET output=README.md
+
 REM Checks if the document exists
-IF NOT EXIST writeup.docx (
-    ECHO Error: No file "writeup.docx" in current directory
+IF NOT EXIST %input% (
+    ECHO Error: No file %input% in current directory
     ECHO Move or rename your file and try again
     GOTO CommonExit
 )
 
 REM Runs the command to update the markdown 
-pandoc writeup.docx ^
+pandoc %input% ^
     --standalone ^
     --extract-media . ^
     --to gfm-smart ^
     --wrap=none ^
     --reference-links ^
-    -o writeup.md
+    -o %output%
 
 REM Check if the command threw any errors
 IF %ERRORLEVEL% NEQ 0 (
@@ -26,7 +29,7 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 
 REM When all is successful
-ECHO Successfully updated writeup.md
+ECHO Successfully updated %output%
 
 :CommonExit
 ECHO.

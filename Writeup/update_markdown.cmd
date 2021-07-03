64b38cf0e5a115bb0898f1df70453b3a8875a44a
Daniel Wait <71983137+daniel95wait@users.noreply.github.com>
2021-07-03 22:44:20 +0100

    Check for Pandoc before clearing media directory

--- Writeup/update_markdown.cmd
+++ Writeup/update_markdown.cmd
@@ -6,19 +6,30 @@ SET input=writeup.docx
 SET output=README.md
 SET media_dir=media
 
-REM Checks if the document exists
+REM Checks if the input file exists
 IF NOT EXIST %input% (
     ECHO Error: No file %input% in current directory
     ECHO Move or rename your file and try again
     GOTO CommonExit
 )
 
+REM Checks if Pandoc is on the path
+REM /q for quiet (just sets ERRORLEVEL, no output)
+where /q pandoc
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Error: Check Pandoc is installed
+    ECHO Pandoc can be downloaded from https://pandoc.org/installing.html
+    GOTO CommonExit
+)
+
 REM Empties old media directory if it exists
 IF EXIST %media_dir% (
-    RMDIR /S /Q %media_dir%
+    REM /s for recursive, /q for quiet (no confirmation)
+    rmdir /s /q %media_dir%
 )
 
 REM Runs the command to update the markdown
+REM Outputs GitHub Favoured Markdown
 pandoc %input% ^
     --standalone ^
     --extract-media . ^
@@ -26,13 +37,6 @@ pandoc %input% ^
     --wrap=none ^
     --reference-links ^
     -o %output%
-
-REM Check if the command threw any errors
-IF %ERRORLEVEL% NEQ 0 (
-    ECHO Error: Check Pandoc is installed
-    ECHO Pandoc can be downloaded from https://pandoc.org/installing.html
-    GOTO CommonExit
-)
 
 REM When all is successful
 ECHO Successfully updated %output%
